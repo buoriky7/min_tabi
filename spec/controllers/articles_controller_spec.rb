c28b9b37b30ff410dda4354c87b9228fe2e7976a
satenmenf <nf.satomi1gou@gmail.com>
2019-06-06 11:38:45 +0900

    [Fix]Clips,Articlesコントローラーの命名を間違えたので削除・新規作成

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ArticleController, type: :controller do
+RSpec.describe ArticlesController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
