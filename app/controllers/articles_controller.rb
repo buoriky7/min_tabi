028bcf92d994415468f041f1b6a5666a9cee6880
satenmenf <nf.satomi1gou@gmail.com>
2019-06-06 17:14:37 +0900

    [Add]Article基本フォーム追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
   def new
+  	@article = Article.new
   end
 
   def edit
+  end
+
+  private
+  def article_params
+  	params.require(:article).permit(:article_image, :caption, :body, )
   end
 end
