3c31f420cdf8148fb3d2469f9cca42bcac9b19a9
satenmenf <nf.satomi1gou@gmail.com>
2019-06-11 19:15:14 +0900

    [Add]article#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def edit
   	@article = Article.find(params[:id])
+    @place = Place.find_by(id: @article.place_id)
   end
 
   def create
@@ -50,6 +51,45 @@ class ArticlesController < ApplicationController
     else
     	flash[:danger] = "記事の投稿に失敗しました"
     	render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.article_icons.each do |article_icon|
+        article_icon.timeline_id = @article.timeline_id
+    end
+    # 以下Placeモデルへの保存処理
+    # Placeモデルが空の時は新規作成
+    if Place.all.empty?
+      @place = Place.new(place_params)
+      @place.user_id = @article.user_id #TODO:user_idがname_error(userとアソシエーション中だと)
+      @place.save
+    else
+      # TODO:もっと良いコードにできないだろうか？
+      # Placeモデルに「current_userと同じuser_id」があればLoad, なければ.new
+      @place = Place.find_or_initialize_by(place_params) #引数がストロングパラメーターだけだと、.newの時に困る
+      if @place.new_record?
+         @place.user_id = current_user.id #TODO:user_idがname_error(userとアソシエーション中だと)
+         @place.save
+      end
+    end
+    # Placeモデルへの保存処理ここまで。以下article保存
+    @article.place_id = @place.id
+    if @article.update(article_params)
+      flash[:success] = "記事を更新しました！"
+      timeline_posted = Timeline.where(user_id: current_user.id).last
+      if timeline_posted.post_flag == 1
+        redirect_to timeline_path(timeline_posted.id)
+      elsif timeline_posted.post_flag == 0
+          redirect_to new_timeline_path(timeline_posted.id)
+      else
+        flash[:danger] = "予期せぬエラーです"
+        redirect_to timelines_path
+      end
+    else
+      flash[:danger] = "記事の更新に失敗しました"
+      render :edit
     end
   end
 
